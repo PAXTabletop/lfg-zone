a799d51d09877a66b82cb3bf69a3a3bf0a53120e
Gail Terman <gterman@gmail.com>
2019-12-06 14:50:11 -0500

    add cookies to make data persist

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Countdown from 'react-countdown-now';
 import './index.css';
+import cookie from 'react-cookies';
 
 function GameDeleteButton ({callback}) {
     return (
@@ -40,7 +41,6 @@ class AddGameForm extends React.Component {
         super(props);
         this.defaultTime = 5
         this.state = {title: '', time: this.defaultTime, table: '', players: ''};
-
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
@@ -61,19 +61,19 @@ class AddGameForm extends React.Component {
         return ( <form onSubmit={this.handleSubmit}>
             <label>
                 Game Title
-                <input required="true" type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+                <input required={true} type="text" name="title" value={this.state.title} onChange={this.handleChange} />
             </label>
             <label>
                 # Players
-                <input required="false" type="text" name="players" value={this.state.players} onChange={this.handleChange} />
+                <input required={false} type="text" name="players" value={this.state.players} onChange={this.handleChange} />
             </label>
             <label>
                 Wait Time
-                <input required="true" type="number" min="1" name="time" value={this.state.time} onChange={this.handleChange} />
+                <input required={true} type="number" min="1" name="time" value={this.state.time} onChange={this.handleChange} />
             </label>
             <label>
                 Table #
-                <input required="true" type="text" name="table" value={this.state.table} onChange={this.handleChange} />
+                <input required={true} type="text" name="table" value={this.state.table} onChange={this.handleChange} />
             </label>
             <input className="submitBtn" type="submit" value="Submit" />
         </form>);
@@ -86,11 +86,13 @@ const fakeGames = [
     {title: 'Game B', time: Date.now() + 10 * 60000, table: '8'}
 ]
 
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {games: fakeGames}
+	const games = cookie.load('games') || [];
+        this.state = {games: games};
         this.gameDeleteCallback = this.gameDeleteCallback.bind(this);
         this.addGame = this.addGame.bind(this);
     }
@@ -99,6 +101,7 @@ class App extends React.Component {
         console.log(`game delete clicked for ${idx}`);
         const newGames = this.state.games.map(g => g);
         newGames.splice(idx, 1);
+	cookie.save('games', newGames);
         this.setState({games: newGames});
     }
 
@@ -107,6 +110,7 @@ class App extends React.Component {
         const newGames = this.state.games.map(g => g);
         time = Date.now() + time * 60000;
         newGames.push({title, time, table, players});
+	cookie.save('games', newGames)
         this.setState({games: newGames});
     }
 
