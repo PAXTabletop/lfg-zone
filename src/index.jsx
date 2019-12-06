408c9cd8f945e70835d71c99366a45e40ac1edc5
Gail Terman <gterman@gmail.com>
2019-12-06 10:01:30 -0500

    number of players option

--- src/index.jsx
+++ src/index.jsx
@@ -9,10 +9,14 @@ function GameDeleteButton ({callback}) {
     );
 }
 
-function Game ({title, time, table, deleteCallback}) {
+function Game ({title, time, table, players, deleteCallback}) {
+    const players_box = players
+        ? <div className='table'>Players: {players}</div>
+        : ''
     return (
         <div className='game'>
             <span className='title'>{title}</span>
+            {players_box}
             <div className='table'>{table}</div>
             <div className='time'><Countdown date={time} /></div>
             <GameDeleteButton callback={deleteCallback} />
@@ -22,7 +26,7 @@ function Game ({title, time, table, deleteCallback}) {
 
 function GameList ({games, deleteCallback}) {
     const renderedGameList = games.map((game, i) => {
-        return <li key={i}><Game title={game.title} time={game.time} table={game.table} deleteCallback={() => deleteCallback(i)} /></li>
+        return <li key={i}><Game title={game.title} time={game.time} table={game.table} players={game.players} deleteCallback={() => deleteCallback(i)} /></li>
     });
     return (
         <ul>
@@ -35,7 +39,7 @@ class AddGameForm extends React.Component {
     constructor(props) {
         super(props);
         this.defaultTime = 5
-        this.state = {title: '', time: this.defaultTime, table: ''};
+        this.state = {title: '', time: this.defaultTime, table: '', players: ''};
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -44,7 +48,7 @@ class AddGameForm extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         this.props.addGame(this.state)
-        this.setState({title: '', time: this.defaultTime, table: ''});
+        this.setState({title: '', time: this.defaultTime, table: '', players: ''});
     }
 
     handleChange(event) {
@@ -58,6 +62,10 @@ class AddGameForm extends React.Component {
             <label>
                 Game Title
                 <input required="true" type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+            </label>
+            <label>
+                # Players
+                <input required="false" type="text" name="players" value={this.state.players} onChange={this.handleChange} />
             </label>
             <label>
                 Wait Time
@@ -74,7 +82,7 @@ class AddGameForm extends React.Component {
 }
 
 const fakeGames = [
-    {title: 'Game A', time: Date.now() + 3 * 60000, table: '3'},
+    {title: 'Game A', time: Date.now() + 3 * 60000, table: '3', players: '4-8'},
     {title: 'Game B', time: Date.now() + 10 * 60000, table: '8'}
 ]
 
@@ -94,11 +102,11 @@ class App extends React.Component {
         this.setState({games: newGames});
     }
 
-    addGame ({title, time, table}) {
-        console.log(`game added: ${title} at table ${table} starting at ${time}`);
+    addGame ({title, time, table, players}) {
+        console.log(`game added: ${title} at table ${table} starting at ${time} wanting players ${players}`);
         const newGames = this.state.games.map(g => g);
         time = Date.now() + time * 60000;
-        newGames.push({title, time, table});
+        newGames.push({title, time, table, players});
         this.setState({games: newGames});
     }
 
